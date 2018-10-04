c39f631e80f36b4ffa74cf3fcbd22d20590a7f25
Matthew Dillon <matthewrdillon@gmail.com>
2018-10-04 10:54:43 -0700

    WIP

--- library/plugins/tests/test_views.py
+++ library/plugins/tests/test_views.py
@@ -1,35 +1,109 @@
 import unittest
+
 from django import test
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+
+from library.plugins.models import Plugin
+
+User = get_user_model()
+
+_BASE_USER = {
+    'email': '',
+    'password': '',
+    'full_name': '',
+    'forum_external_id': '',
+    'forum_avatar_url': 'https://qiime2.org',
+    'forum_is_admin': False,
+    'forum_is_moderator': False,
+}
 
 
-class AnonymousUserAuthorizationTests(unittest.TestCase):
+_BASE_PLUGIN = {
+    'title': 'plugin',
+    'short_summary': 'lorem ipsum summary',
+    'description': 'lorem ipsum description',
+    'install_guide': 'lorem ipsum install',
+    'published': True,
+    'source_url': 'https://qiime2.org',
+    'version': '0.1.4',
+}
+
+
+class AnonymousUserAuthorizationTests(test.TestCase):
+    # @classmethod
+    # def setUpTestData(cls):
+    #     cls.author = User.objects.create_user('author', **{**_BASE_USER, 'forum_external_id': '1'})
+    #     cls.author.groups.add(Group.objects.get(name='forum_trust_level_1'))
+    #     cls.anon = User.objects.create_user('anon', **{**_BASE_USER, 'forum_external_id': '2'})
+
+    #     cls.published_plugin_1 = Plugin.unsafe.create(
+    #         **{**_BASE_PLUGIN, 'title': 'published_plugin_1'})
+    #     cls.published_plugin_2 = Plugin.unsafe.create(
+    #         **{**_BASE_PLUGIN, 'title': 'published_plugin_2'})
+    #     cls.unpublished_plugin = Plugin.unsafe.create(
+    #         **{**_BASE_PLUGIN, 'title': 'unpublished_plugin', 'published': False})
+
     def setUp(self):
         self.client = test.Client()
 
     def test_plugin_list_no_unpublished(self):
+        Plugin.unsafe.create(**{**_BASE_PLUGIN, 'title': 'published_plugin_1'})
+        Plugin.unsafe.create(**{**_BASE_PLUGIN, 'title': 'published_plugin_2'})
+
         response = self.client.get('/plugins/')
 
         self.assertEqual(response.status_code, 200)
-        # TODO: Add in plugins into test DB
         self.assertEqual(len(response.context['plugins']), 2)
 
     def test_plugin_list_some_unpublished(self):
-        pass
+        Plugin.unsafe.create(**{**_BASE_PLUGIN, 'title': 'published_plugin_1'})
+        Plugin.unsafe.create(**{**_BASE_PLUGIN, 'title': 'published_plugin_2'})
+        Plugin.unsafe.create(
+                **{**_BASE_PLUGIN, 'title': 'unpublished_plugin', 'published': False})
+
+        response = self.client.get('/plugins/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['plugins']), 2)
 
     def test_plugin_detail_unpublished(self):
-        pass
+        plugin = Plugin.unsafe.create(
+                **{**_BASE_PLUGIN, 'title': 'unpublished_plugin', 'published': False})
+
+        response = self.client.get('/plugins/%s/' % plugin.slug)
+
+        self.assertEqual(response.status_code, 404)
 
     def test_plugin_detail_published(self):
-        pass
+        plugin = Plugin.unsafe.create( **{**_BASE_PLUGIN, 'title': 'published_plugin'})
+
+        response = self.client.get('/plugins/%s/%d/' % (plugin.slug, plugin.id))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['plugin'], plugin)
 
     def test_plugin_new(self):
-        pass
+        response = self.client.get('/plugins/new/')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/login/?next=/plugins/new/')
 
     def test_plugin_edit_unpublished(self):
-        pass
+        plugin = Plugin.unsafe.create(
+                **{**_BASE_PLUGIN, 'title': 'unpublished_plugin', 'published': False})
+
+        response = self.client.get('/plugins/%s/edit/' % plugin.slug)
+
+        self.assertEqual(response.status_code, 404)
 
     def test_plugin_edit_published(self):
-        pass
+        plugin = Plugin.unsafe.create(
+                **{**_BASE_PLUGIN, 'title': 'published_plugin'})
+
+        response = self.client.get('/plugins/%s/edit/' % plugin.slug)
+
+        self.assertEqual(response.status_code, 404)
 
 
 class LoggedInUserAuthorizationTests(unittest.TestCase):
