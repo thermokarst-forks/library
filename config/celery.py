c4c89a9a831daf11ddf29378e0602fd1b035fe5f
Matthew Dillon <matthewrdillon@gmail.com>
2020-05-20 14:12:23 -0700

    squash

--- config/celery.py
+++ config/celery.py
@@ -1,6 +1,6 @@
 from celery import Celery
 
 
-app = Celery('library-tasks', backend='rpc://', broker='rabbitmq')
+app = Celery('library-tasks', backend='rpc://', broker='amqp://')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
