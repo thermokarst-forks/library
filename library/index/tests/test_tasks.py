26ec44c17067536d64c9325a61c4d8c9e9193ac5
Matthew Dillon <matthewrdillon@gmail.com>
2020-05-21 07:45:23 -0700

    tweak test suite

--- library/index/tests/test_tasks.py
+++ library/index/tests/test_tasks.py
@@ -1,8 +1,12 @@
 from django import test
+from django.test.utils import override_settings
+
 from library.index.tasks import debug
 
 
 class DebugTestCase(test.TestCase):
+    @override_settings(CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
+                       CELERY_ALWAYS_EAGER=True)
     def test_debug_task(self):
         result = debug.delay({'hello': 'world'})
         self.assertTrue(result.successful())
--- library/index/tests/test_views.py
+++ library/index/tests/test_views.py
@@ -1,10 +1,14 @@
 from django import test
 
+from django.test.utils import override_settings
+
 
 class ViewTests(test.TestCase):
     def setUp(self):
         self.client = test.Client()
 
+    @override_settings(CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
+                       CELERY_ALWAYS_EAGER=True)
     def test_debug_celery_view(self):
         response = self.client.get('/_debug?hello=world')
         self.assertEqual(response.status_code, 200)
