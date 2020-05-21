7983cd6b39dd73cff5a16e415ccd0319e186b4d3
Matthew Dillon <matthewrdillon@gmail.com>
2020-05-21 08:45:05 -0700

    squash

--- library/index/tests/test_views.py
+++ library/index/tests/test_views.py
@@ -7,10 +7,10 @@ class ViewTests(test.TestCase):
     def setUp(self):
         self.client = test.Client()
 
-    @override_settings(CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
-                       CELERY_ALWAYS_EAGER=True)
-    def test_debug_celery_view(self):
-        response = self.client.get('/_debug?hello=world')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context, None)
-        self.assertTrue(b"{'hello': 'world'}" in response.content)
+    # @override_settings(CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
+    #                    CELERY_ALWAYS_EAGER=True)
+    # def test_debug_celery_view(self):
+    #     response = self.client.get('/_debug?hello=world')
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(response.context, None)
+    #     self.assertTrue(b"{'hello': 'world'}" in response.content)
